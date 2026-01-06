3d1fc9e6347010f0871e2cc98ce96d5c92f41008
Lidia Korovkina <77280639+lidiaakorovkinaa@users.noreply.github.com>
2026-01-07 02:07:03 +0300

    Delete myProject directory

--- myProject/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path
-from .views import hello_page
-
-urlpatterns = [
-    path('', hello_page),
-]
--- myProject/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def hello_page(request):
-    return HttpResponse("<h1>Hello, World</h1>")
